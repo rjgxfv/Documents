840e12db9aedb96d46bcc6dba50c7ab52d216ab8
Robert Graman <rjgxfv@mail.missouri.edu>
2019-02-19 11:54:22 -0600

    added Core Data implementation

---
+++ Documents/Doc+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Doc+CoreDataClass.swift
+//  Documents
+//
+//  Created by Robert Graman on 2/19/19.
+//  Copyright © 2019 Robert Graman. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Doc)
+public class Doc: NSManagedObject {
+
+}
